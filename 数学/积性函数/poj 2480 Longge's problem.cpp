4386e794b26cf7659e480a14dea0610f22418ee6
yejinru <545883134@qq.com>
2013-03-11 13:24:07 +0800

    update

---
+++ 数学/积性函数/poj 2480 Longge's problem.cpp
@@ -0,0 +1,105 @@
+/*
+
+题目：求sigma( gcd(i,n) )
+分析：gcd(i,n) = gcd(i,a*b) = gcd(i,a)*gcd(i,b)
+    所以gcd是一个积性函数。
+    积性函数的和还是积性函数。
+    所以
+    f(n) = sigma( gcd(i,n) )也是积性函数
+    f(n) = sigma( d* ph(n/d) ),i = d|n
+    所以可以化为
+    f(n) = f(p1^k1) * f(p2^k2) *...* f(pm^km)
+    f(p^k) = sigma( p^i * ph( p^(k-i) ) )
+    而
+    ph(p^k) = p^k-p^(k-1)
+    所以问题得以解决
+
+*/
+#include <vector>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define debug puts("here")
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define REP(i,a,b) for(int i=a;i<b;i++)
+#define foreach(i,vec) for(unsigned i=0;i<vec.size();i++)
+#define pb push_back
+#define RD(n) scanf("%d",&n)
+#define RD2(x,y) scanf("%d%d",&x,&y)
+#define RD3(x,y,z) scanf("%d%d%d",&x,&y,&z)
+
+const int MAXN = 50000;
+
+vector<ull> prime;
+bool use[MAXN];
+ull n;
+
+void init(){
+    memset(use,false,sizeof(use));
+    for(int i=2;i<MAXN;i++)
+        if(use[i]==false){
+            prime.pb(i);
+            for(int j=i+i;j<MAXN;j+=i)
+                use[j] = true;
+        }
+}
+
+ull pw(ull p,int k){
+    if(k==0)    return 1;
+    ull ans = p-1;
+
+    while(--k)
+        ans *= p;
+    return ans;
+}
+
+ull fpk(ull p,int k){
+    ull ans = 0;
+    ull t = 1;
+    for(int i=0;i<=k;i++){
+        ans += t*pw(p,k-i);
+        t *= p;
+    }
+    return ans;
+}
+
+void solve(){
+    ull ans = 1;
+    for(int i=0;prime[i]*prime[i]<=n;i++){
+        ull p = prime[i];
+        if(n%p)
+            continue;
+        int k = 0;
+        while(n%p==0){
+            n /= p;
+            k ++;
+        }
+        ans *= fpk(p,k);
+    }
+    if(n>1)
+        ans *= fpk(n,1);
+    cout<<ans<<endl;
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("sum.in","r",stdin);
+	//freopen("sum.out","w",stdout);
+#endif
+
+    init();
+    while(cin>>n)
+        solve();
+
+	return 0;
+}
+
